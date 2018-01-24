20fbf71b826f11ba3ea30563bbad32ed1d7ab414
Nowak Karol <KNowak22@corpo.t-mobile.pl>
2018-01-24 12:21:20 +0100

    zmiana4

--- blackJ_24_01.cpp
+++ blackJ_24_01.cpp
@@ -35,7 +35,7 @@ class Gracz {
 
 	int losowe_dobieranie(){
 	temp2 = rand()%5;
-	cout << temp2 << endl;
+	// cout << temp2 << endl; TEST
 	}
 };
 
@@ -43,7 +43,7 @@ class Gracz {
 class Karty {
     public:
     int temp;
-    string wartosc[104]={"1W2","1W3","1W4","1W5","1W6","1W7","1W8","1W9","1W10","1WJ","1WD","1WK","1WA","1Z2","1Z3","1Z4","1Z5","1Z6","1Z7","1Z8","1Z9","1Z10","1ZJ","1ZD","1ZK","1ZA","1D2","1D3","1D4","1D5","1D6","1D7","1D8","1D9","1D10","1DJ","1DD","1DK","1DA","1C2","1C3","1C4","1C5","1C6","1C7","1C8","1C9","1C10","1CJ","1CD","1CK","1CA","2W2","2W3","2W4","2W5","2W6","2W7","2W8","2W9","2W20","2WJ","2WD","2WK","2WA","2Z2","2Z3","2Z4","2Z5","2Z6","2Z7","2Z8","2Z9","2Z20","2ZJ","2ZD","2ZK","2ZA","2D2","2D3","2D4","2D5","2D6","2D7","2D8","2D9","2D20","2DJ","2DD","2DK","2DA","2C2","2C3","2C4","2C5","2C6","2C7","2C8","2C9","2C20","2CJ","2CD","2CK","2CA"};
+    string wartosc[104]={"1W2","1W3","1W4","1W5","1W6","1W7","1W8","1W9","1W10","1WJ","1WD","1WK","1WA","1Z2","1Z3","1Z4","1Z5","1Z6","1Z7","1Z8","1Z9","1Z10","1ZJ","1ZD","1ZK","1ZA","1D2","1D3","1D4","1D5","1D6","1D7","1D8","1D9","1D10","1DJ","1DD","1DK","1DA","1C2","1C3","1C4","1C5","1C6","1C7","1C8","1C9","1C10","1CJ","1CD","1CK","1CA","2W2","2W3","2W4","2W5","2W6","2W7","2W8","2W9","2W10","2WJ","2WD","2WK","2WA","2Z2","2Z3","2Z4","2Z5","2Z6","2Z7","2Z8","2Z9","2Z10","2ZJ","2ZD","2ZK","2ZA","2D2","2D3","2D4","2D5","2D6","2D7","2D8","2D9","2D10","2DJ","2DD","2DK","2DA","2C2","2C3","2C4","2C5","2C6","2C7","2C8","2C9","2C10","2CJ","2CD","2CK","2CA"};
 	int wartosc_karty[104]={2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11};
 	string karta;
 
@@ -61,10 +61,10 @@ class Karty {
 class Gra {
 	public:
 
-	int sprawdzenie(int a, int b){			// DODAC ZE JAK KRUPIER MA PONAD 21 TO WYGRYWAM
+	int sprawdzenie(int a, int b){
 		if(a==b){
 		cout << "Remis" << endl << endl;
-		cout << "Jeszcze raz? (y/n)"<< endl;			// JESZCZE RAZ ROZBIC NA ODDZIELNA FUNKCJE2
+		cout << "Jeszcze raz? (y/n)"<< endl;			
 		cin >> wybor2;
 		} else if((a>b || b>21) && a<=21) {				// wygrana jak krupier ma wiecej niz 21 b>21
 		  cout << "Wygrales" << endl;
@@ -77,13 +77,6 @@ class Gra {
 			cin >> wybor2;
 		}
 	}
-
-	//int przegrana(){
-	//cout << "Przegrales!"<< endl;
-	//cout << "Jeszcze raz? (y/n)"<< endl;			// JESZCZE RAZ ROZBIC NA ODDZIELNA FUNKCJE2
-	//cin >> wybor2;
-	//}
-
 };
 
 
@@ -124,16 +117,16 @@ int main(){
     cout << "Karta 1: " << talia1.karta << endl;
 	gracz.posiadane_karty[j]=talia1.karta;
 	j++;
-	// cout << talia1.temp << endl;										// nr karty w tab string_warosc
-	// cout << "Punkty za karte: " << talia1.ktora_karta << endl;
+	// cout << talia1.temp << endl;										// TEST - nr karty w tab string_warosc
+	// cout << "Punkty za karte: " << talia1.ktora_karta << endl;			TEST
     gracz.punkty+=talia1.ktora_karta;
 	talia1.rozdanie();
 	while (talia1.ktora_karta==0)  talia1.rozdanie();
 	cout << "Karta 2: " << talia1.karta << endl;
 	gracz.posiadane_karty[j]=talia1.karta;
 	j++;
-	// cout << talia1.temp << endl;										// nr karty w tab string_warosc
-	// cout << "Punkty za karte: " << talia1.ktora_karta << endl;
+	// cout << talia1.temp << endl;										// TEST - nr karty w tab string_warosc
+	// cout << "Punkty za karte: " << talia1.ktora_karta << endl;			TEST
     gracz.punkty+=talia1.ktora_karta;
 	gracz.wyswietl_pkt();
 
@@ -150,9 +143,10 @@ int main(){
     krupier.punkty+=talia1.ktora_karta;
 	krupier.posiadane_karty[k]=talia1.karta;
 	k++;
+	cout << endl;
     //cout << talia1.karta << endl; // test
-	cout<< "TEST: ";
-    krupier.wyswietl_pkt(); // TEST - USUNAC TO
+	//cout<< "TEST: ";	
+    // krupier.wyswietl_pkt(); // TEST - ilosc punktow krupiera
 
 		for(;;){
 
@@ -207,7 +201,7 @@ int main(){
 							krupier.posiadane_karty[k]=talia1.karta;
 							k++;
 							//cout << talia1.karta << endl; // test
-							cout << "TEST: ";
+							// cout << "TEST: ";
 							krupier.wyswietl_pkt();
 
 				}
@@ -273,7 +267,7 @@ int main(){
 									krupier.wyswietl_pkt();
 								} else cout << "Krupier nie dobiera karty" << endl;
 							}
-							gra.sprawdzenie(gracz.punkty, krupier.punkty);
+							//gra.sprawdzenie(gracz.punkty, krupier.punkty);
 												
 								
 								break;
@@ -301,7 +295,7 @@ int main(){
 					gracz.wyswietl_pkt();
 					cout << "KRUPIER" << endl;
 					krupier.wyswietl_karty(k);
-					cout << "TEST: ";
+					// cout << "TEST: ";
 					krupier.wyswietl_pkt();
 
 
